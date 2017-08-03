c63515bdf3e525c89eec9f3b54a6b28dd5a70bb4
Daniel Cardeña Pulido <dcardena@bravent.net>
2017-08-03 09:08:14 +0200

    First upload

---
+++ DualCameraTest/BackCameraPreview.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using Android.Content;
+using Android.Hardware;
+using Android.Views;
+
+namespace DualCameraTest
+{
+    public class BackCameraPreview : SurfaceView, ISurfaceHolderCallback
+    {
+        public static string TAG = "BackCameraPreview";
+
+        ISurfaceHolder mHolder;
+        Camera mCamera;
+
+        public BackCameraPreview(Context context, Camera camera) : base(context)
+        {
+            mCamera = camera;
+
+            // Install a SurfaceHolder.Callback so we get notified when the
+            // underlying surface is created and destroyed.
+            mHolder = Holder;
+            mHolder.AddCallback(this);
+            mHolder.SetType(SurfaceType.PushBuffers);
+        }
+
+        public void SurfaceCreated(ISurfaceHolder holder)
+        {
+            // The Surface has been created, acquire the camera and tell it where
+            // to draw.
+            try
+            {
+                if (mCamera != null)
+                {
+                    mCamera.SetPreviewDisplay(holder);
+                    mCamera.StartPreview();
+                }
+            }
+            catch (Java.IO.IOException exception)
+            {
+                //Log.Error(TAG, "IOException caused by setPreviewDisplay()", exception);
+            }
+        }
+
+        public void SurfaceDestroyed(ISurfaceHolder holder)
+        {
+
+        }
+
+        public void SurfaceChanged(ISurfaceHolder holder, Android.Graphics.Format format, int w, int h)
+        {
+            if (Holder == null)
+            {
+                // preview surface does not exist
+                return;
+            }
+
+            // stop preview before making changes
+            try
+            {
+                mCamera.StopPreview();
+            }
+            catch (Exception e)
+            {
+                // ignore: tried to stop a non-existent preview
+            }
+
+            // set preview size and make any resize, rotate or
+            // reformatting changes here
+
+            // start preview with new settings
+            try
+            {
+                mCamera.SetPreviewDisplay(mHolder);
+                mCamera.StartPreview();
+
+            }
+            catch (Exception e)
+            {
+                //Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+            }
+        }
+    }
+}
---
+++ DualCameraTest/MainActivity.cs
@@ -0,0 +1,66 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Hardware;
+using Android.Views;
+using System.Collections.Generic;
+using Android.Content;
+using System;
+using Android.Util;
+
+namespace DualCameraTest
+{
+    [Activity(Label = "DualCameraTest", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        private Camera mBackCamera;
+        private Camera mFrontCamera;
+        private BackCameraPreview mBackCamPreview;
+        private FrontCameraPreview mFrontCamPreview;
+
+        public static String TAG = "DualCamActivity";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Activity_Dual_Cam);
+
+            Log.Info(TAG, "Number of cameras: " + Camera.NumberOfCameras);
+
+            // Create an instance of Camera
+            mBackCamera = getCameraInstance(0);
+            // Create back camera Preview view and set it as the content of our activity.
+            mBackCamPreview = new BackCameraPreview(this, mBackCamera);
+            FrameLayout backPreview = (FrameLayout)FindViewById(Resource.Id.back_camera_preview);
+            backPreview.AddView(mBackCamPreview);
+
+            mFrontCamera = getCameraInstance(1);
+            mFrontCamPreview = new FrontCameraPreview(this, mFrontCamera);
+            FrameLayout frontPreview = (FrameLayout)FindViewById(Resource.Id.front_camera_preview);
+            frontPreview.AddView(mFrontCamPreview);
+
+
+        }
+        public override bool OnPrepareOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.camera_menu, menu);
+            return base.OnPrepareOptionsMenu(menu);
+        }
+
+        public static Camera getCameraInstance(int cameraId)
+        {
+            Camera c = null;
+            try
+            {
+                c = Camera.Open(cameraId); // attempt to get a Camera instance
+            }
+            catch (Exception e)
+            {
+                // Camera is not available (in use or does not exist)
+                Log.Error(TAG, "Camera " + cameraId + " not available! " + e);
+            }
+            return c; // returns null if camera is unavailable
+        }
+    }
+}
+
